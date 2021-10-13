7c558e0e898d39320ce3187883b20bdfc76eb037
Alek <aleksebastiano@gmail.com>
2021-10-12 22:36:00 -0500

    Added layout styling and placeholder text for resume

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,2 +1,44 @@
-<h1>Resume</h1>
-<p>Resume info goes here</p>
+<div class="flex flex-col items-center">
+	<h1 class="text-5xl">Alek Ortiz</h1>
+	<span>Title goes here</span>
+	<span>Contact info goes here</span>
+</div>
+<div class="grid gap-4 lg:grid-cols-2">
+	<h1 class="text-5xl col-start-1">About me</h1>
+	<span class="col-start-1">About me goes here</span>
+
+	<h1 class="text-5xl col-start-1">Experience</h1>
+	<span class="text-3xl col-start-1">Title and dates</span>
+	<span class="col-start-1">Location</span>
+	<span class="col-start-1">Name of Projects</span>
+	<span class="col-start-1">Tech Stack</span>
+	<ul class="col-start-1 list-disc list-inside">
+		<li>Job description bullet points go here</li>
+		<li>Job description bullet points go here</li>
+		<li>Job description bullet points go here</li>
+	</ul>
+
+	<span class="text-3xl col-start-1">Title and dates</span>
+	<span class="col-start-1">Location</span>
+	<span class="col-start-1">Name of Projects</span>
+	<span class="col-start-1">Tech Stack</span>
+	<ul class="col-start-1 list-disc list-inside">
+		<li>Job description bullet points go here</li>
+		<li>Job description bullet points go here</li>
+		<li>Job description bullet points go here</li>
+	</ul>
+
+	<h1 class="text-5xl col-start-1">Education</h1>
+	<span class="text-2xl col-start-1">Name of school and dates go here</span>
+	<span class="col-start-1">Education details go here</span>
+
+	<span class="text-2xl col-start-1">Name of school and dates go here</span>
+	<span class="col-start-1">Education details go here</span>
+
+	<h1 class="text-5xl sm:col-start-1 lg:col-start-2 lg:row-start-1">Skills</h1>
+	<ul class="list-disc list-inside lg:col-start-2 lg:row-start-2 ">
+		<li>Skills go here</li>
+		<li>Skills go here</li>
+		<li>Skills go here</li>
+	</ul>
+</div>
