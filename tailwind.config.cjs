cbe28097bb7f3f3a1d7067ae1f317ea1fe58135c
Alek <aleksebastiano@gmail.com>
2021-10-12 22:35:38 -0500

    Added custom grid template rule

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,11 @@
 module.exports = {
 	mode: 'jit',
-	purge: ['./src/**/*.svelte']
+	purge: ['./src/**/*.svelte'],
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				2: '3fr 1fr'
+			}
+		}
+	}
 };
