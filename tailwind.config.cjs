dcc2a61db49034091e79cffb564b6c40f901de01
Alek <aleksebastiano@gmail.com>
2021-10-17 01:34:35 -0500

    Added custom rules

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,23 @@ module.exports = {
 		extend: {
 			gridTemplateColumns: {
 				2: '6fr 1fr'
+			},
+			minHeight: {
+				0: '0',
+				'1/4': '25%',
+				'1/2': '50%',
+				'3/4': '75%',
+				full: '100%'
+			},
+			maxHeight: {
+				0: '0',
+				'1/4': '25%',
+				'1/2': '50%',
+				'3/4': '75%',
+				full: '100%'
+			},
+			transitionProperty: {
+				top: 'top'
 			}
 		}
 	}
