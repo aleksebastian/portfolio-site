fce60f533f64f61bbad6388b8c706362ac2c0188
Alek <aleksebastiano@gmail.com>
2021-10-18 16:44:35 -0500

    Removed unnecessary imports and added desired functionality

--- src/components/projects/projects.svelte
+++ src/components/projects/projects.svelte
@@ -1,15 +1,19 @@
 <script>
+	// import { onMount } from 'svelte';
+	// import { writable } from 'svelte/store';
+	// import { fade } from 'svelte/transition';
+	// import fetchRepos from '../../stores/repos';
 	import Card from './card.svelte';
+	export let repos;
 </script>
 
-<div class="flex gap-16 justify-center flex-wrap items-stretch bg-red-300">
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
+<div class="flex flex-col items-center">
+	<h1 class="text-4xl mb-20">Projects</h1>
+	<div class="flex gap-16 justify-center flex-wrap ">
+		{#each repos as repo}
+			<Card {repo} />
+		{:else}
+			<p>loading...</p>
+		{/each}
+	</div>
 </div>
