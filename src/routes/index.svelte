a77811c16c196718fa6da99a35e6696052864f01
Alek <aleksebastiano@gmail.com>
2021-10-18 16:50:42 -0500

    Added module script with load function to enable SSR  and added projects component to landing page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,30 @@
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const username = 'aleksebastian';
+		const url = `https://api.github.com/users/${username}/repos`;
+		const res = await fetch(url);
+		const data = await res.json();
+		const reposInfo = data
+			.filter((repo) => repo.topics.includes('portfolio-project'))
+			.map((repo) => ({
+				name: repo.name,
+				url: repo.html_url,
+				description: repo.description
+			}));
+		return { props: { repos: reposInfo } };
+	};
+</script>
+
 <script>
 	import { fade } from 'svelte/transition';
 	import Projects from '../components/projects/projects.svelte';
+	export let repos;
 </script>
 
 <svelte:head>
 	<title>Alek Ortiz's Portfolio</title>
 </svelte:head>
 
-<h1>Hello</h1>
-<h1 in:fade>Projects go here</h1>
-<Projects />
+<div in:fade>
+	<Projects {repos} />
+</div>
