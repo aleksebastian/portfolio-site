a2bc925d523020d81bcb2dca77d8c3ca8d66db4c
Alek <aleksebastiano@gmail.com>
2021-10-18 16:48:25 -0500

    Imported navs to render depending on screen size and onmount to prevent wrong style flashing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,25 @@
 <script>
-	import Nav from '../components/nav.svelte';
+	import MobileNav from '../components/nav/mobileNav.svelte';
+	import Nav from '../components/nav/nav.svelte';
+	import MediaQuery from '../components/mediaQuery.svelte';
+	import { onMount } from 'svelte';
+
+	let ready = false;
+
+	onMount(() => (ready = true));
 </script>
 
-<div class="p-8 max-w-6xl mx-auto">
-	<Nav />
-	<slot />
+<div class="p-8 max-w-6xl mx-auto overflow-x-hidden font-body">
+	{#if ready}
+		<MediaQuery query="(max-width: 480px)" let:matches>
+			{#if matches}
+				<MobileNav />
+			{:else}
+				<Nav />
+			{/if}
+		</MediaQuery>
+		<slot />
+	{/if}
 </div>
 
 <style>
