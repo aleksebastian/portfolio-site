293c1955aa5bbd6fd43542d1928f54400a33cdf8
Alek <aleksebastiano@gmail.com>
2021-10-18 16:46:19 -0500

    Refactored some styles to work better for mobile in resume header

--- src/components/resume/header.svelte
+++ src/components/resume/header.svelte
@@ -1,13 +1,13 @@
 <div class="flex flex-col items-center mb-10">
-	<h1 class="text-5xl mb-3">Alek Ortiz</h1>
-	<span class="text-sm sm:text-xl">Software Engineer | Full-Stack Web Developer</span>
-	<div class="flex flex-col md:flex-row">
+	<h1 class="text-5xl mb-2">Alek Ortiz</h1>
+	<span class="text-sm sm:text-xl mb-3">Software Engineer | Full-Stack Web Developer</span>
+	<div class="flex flex-wrap justify-center sm:flex-row">
 		<span class="pr-1"
 			><a
 				class="underline text-blue-500 hover:text-blue-700 visited:text-purple-500"
 				href="https://aleksebastian.dev/"
 				target="_blank">aleksebastian.dev</a
-			> <span class="invisible md:visible">|</span></span
+			> <span class=" sm:visible">|</span></span
 		>
 		<span class="pr-1"
 			>Github <a
@@ -15,21 +15,21 @@
 				href="https://github.com/aleksebastian"
 				target="_blank">@aleksebastian</a
 			>
-			<span class="invisible md:visible">|</span></span
+			<span class=" sm:visible">|</span></span
 		>
 		<span class="pr-1"
 			><a
 				class="underline text-blue-500 hover:text-blue-700 visited:text-purple-500"
 				href="https://linkedin.com/in/alek-ortiz/"
 				target="_blank">LinkedIn</a
-			> <span class="invisible md:visible">|</span></span
+			> <span class="sm:visible">|</span></span
 		>
 		<span class="pr-1"
 			><a
 				class="underline text-blue-500 hover:text-blue-700 visited:text-purple-500"
 				href="mailto:aleksebastian@outlook.com"
 				target="_blank">aleksebastian</a
-			> at outlook</span
+			> @outlook</span
 		>
 	</div>
 </div>
