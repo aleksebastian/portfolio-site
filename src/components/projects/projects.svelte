d1ca69895c715af7e43f2c204fe614ae34d4aa2b
Alek <aleksebastiano@gmail.com>
2021-10-18 20:50:08 -0500

    Removed unused imports and added transition to component instead of index file

--- src/components/projects/projects.svelte
+++ src/components/projects/projects.svelte
@@ -1,13 +1,10 @@
 <script>
-	// import { onMount } from 'svelte';
-	// import { writable } from 'svelte/store';
-	// import { fade } from 'svelte/transition';
-	// import fetchRepos from '../../stores/repos';
+	import { fade } from 'svelte/transition';
 	import Card from './card.svelte';
 	export let repos;
 </script>
 
-<div class="flex flex-col items-center">
+<div in:fade class="flex flex-col items-center">
 	<h1 class="text-4xl mb-20">Projects</h1>
 	<div class="flex gap-16 justify-center flex-wrap ">
 		{#each repos as repo}
