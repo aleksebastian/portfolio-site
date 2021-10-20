5e0ca0d5641fe05b29360db31c9c734b66beddcd
Alek <aleksebastiano@gmail.com>
2021-10-19 21:21:03 -0500

    Added more text to resume about me section

--- src/components/resume/aboutMe.svelte
+++ src/components/resume/aboutMe.svelte
@@ -1,5 +1,7 @@
 <h1 class="text-3xl md:text-4xl col-start-1 mb-5">About me</h1>
-<span class="col-start-1"
-	>I am a full-stack software engineer with 2 years of experience in JavaScript, Node.js, and strong
-	skills using the MERN stack.</span
->
+<span class="col-start-1">
+	I am a Full-stack software engineer with two years of experience in JavaScript, Node.js, and a
+	strong background utilizing the MERN stack. I am passionate about my work and enjoy constantly
+	learning new things. When I'm away from my desk, you can find me playing sports, at the gym, or
+	trying out new restaurants with my friends and family.
+</span>
