773a269b7420fbc5cb03e56bd98493af15a2d141
Alek <aleksebastiano@gmail.com>
2021-10-19 21:22:41 -0500

    Added CSS transition to links

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -10,21 +10,21 @@
 	<title>Contact</title>
 </svelte:head>
 
-<div in:fade class="flex flex-col gap-10 items-center h-screen">
+<div in:fade class="flex flex-col gap-10 items-center">
 	<h1 class="text-4xl sm:-ml-5">Get in touch</h1>
 	<div class="flex gap-5 flex-col">
 		<a
-			class="flex gap-3"
+			class="group flex gap-3"
 			aria-label="Link to portfolio website"
 			href="https://aleksebastian.dev/"
 			rel="noopener"
 			target="_blank"
 		>
-			<div class="hover:opacity-90 h-8 w-8">
+			<div class="transition ease-in-out group-hover:opacity-90 h-8 w-8">
 				<FaUser />
 			</div>
 			<a
-				class="self-center text-blue-500 hover:text-blue-700 visited:text-purple-500"
+				class="self-center text-blue-500 transition ease-in-out group-hover:text-blue-700 visited:text-purple-500"
 				aria-label="Link to portfolio website"
 				href="https://aleksebastian.dev/"
 				rel="noopener"
@@ -33,17 +33,17 @@
 		</a>
 
 		<a
-			class="flex gap-3"
+			class="group flex gap-3"
 			aria-label="Link to github"
 			href="https://github.com/aleksebastian"
 			rel="noopener"
 			target="_blank"
 		>
-			<div class="hover:opacity-90 h-8 w-8">
+			<div class="transition ease-in-out group-hover:opacity-90 h-8 w-8">
 				<FaGithubSquare />
 			</div>
 			<a
-				class="self-center text-blue-500 hover:text-blue-700 visited:text-purple-500"
+				class="self-center text-blue-500 transition ease-in-out group-hover:text-blue-700 visited:text-purple-500"
 				aria-label="Link to github"
 				href="https://github.com/aleksebastian"
 				rel="noopener"
@@ -52,17 +52,17 @@
 		</a>
 
 		<a
-			class="flex gap-3"
+			class="group flex gap-3"
 			aria-label="Link to linkedin"
 			href="https://linkedin.com/in/alek-ortiz/"
 			rel="noopener"
 			target="_blank"
 		>
-			<div class="hover:opacity-90 h-8 w-8">
+			<div class="transition ease-in-out group-hover:opacity-90 h-8 w-8">
 				<FaLinkedin />
 			</div>
 			<a
-				class="self-center text-blue-500 hover:text-blue-700 visited:text-purple-500"
+				class="self-center text-blue-500 transition ease-in-out group-hover:text-blue-700 visited:text-purple-500"
 				aria-label="Link to linkedin"
 				href="https://linkedin.com/in/alek-ortiz/"
 				rel="noopener"
@@ -71,17 +71,17 @@
 		</a>
 
 		<a
-			class="flex gap-3"
+			class="group flex gap-3 transition-colors duration-500 ease-in-out"
 			aria-label="Link to email"
 			href="mailto:aleksebastian@outlook.com"
 			rel="noopener"
 			target="_blank"
 		>
-			<div class="hover:opacity-90 h-8 w-8">
+			<div class="transition ease-in-out group-hover:opacity-90 h-8 w-8">
 				<FaEnvelopeSquare />
 			</div>
 			<a
-				class="self-center text-blue-500 hover:text-blue-700 visited:text-purple-500"
+				class="self-center text-blue-500 transition ease-in-out group-hover:text-blue-700 visited:text-purple-500"
 				aria-label="Link to email"
 				href="mailto:aleksebastian@outlook.com"
 				rel="noopener"
