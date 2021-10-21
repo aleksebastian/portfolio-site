05a2ddb4bd08f920e35c71ae0144fe4d0d7a3622
Alek <aleksebastian@gmail.com>
2021-10-21 00:45:39 -0500

    Added real info for card and refactored css in style tag to tailwind

--- src/components/projects/card.svelte
+++ src/components/projects/card.svelte
@@ -3,28 +3,26 @@
 	export let repo;
 </script>
 
-<div
+<a
 	in:fly={{ y: 200, duration: 1000 }}
-	class="group flex flex-col justify-end w-72 h-72 overflow-hidden relative rounded"
-	style="background-image: url('https://picsum.photos/288');"
+	class="group flex flex-col justify-end w-72 h-72 overflow-hidden relative rounded
+		background-white bg-mockup bg-cover bg-no-repeat"
+	style="background-image: url('https://raw.githubusercontent.com/{repo.username}/{repo.name}/main/mockup.webp');"
+	href={repo.url}
+	rel="noopener"
+	target="_blank"
+	aria-label="Link to github repository"
 >
 	<div />
 	<div
-		class="slider w-full h-full xl:top-full group-hover:top-0 transition-top duration-500 ease-in-out  absolute flex flex-col justify-end p-4"
+		class="bg-gradient-to-t from-black w-full h-full xl:top-full group-hover:top-0 transition-top duration-500 ease-in-out absolute flex flex-col justify-end p-4"
 	>
-		<h1 class="mt-6 text-2xl text-white mb-2">
+		<h1 class=" text-2xl text-white mb-2">
 			{repo.name}
 		</h1>
-		<p class=" text-white">
+		<p class=" text-white mb-2">
 			{repo.description}
 		</p>
+		<p class="text-sm text-gray-400 -mb-2">Click to go to Github repo</p>
 	</div>
-</div>
-
-<style>
-	.slider {
-		background: -moz-linear-gradient(top, rgba(0, 0, 0, 0.65) 0%, rgba(0, 0, 0, 0) 100%);
-		background: -webkit-linear-gradient(top, rgba(0, 0, 0, 0.65) 0%, rgba(0, 0, 0, 0) 100%);
-		background: linear-gradient(to top, rgba(0, 0, 0, 0.65) 0%, rgba(0, 0, 0, 0) 100%);
-	}
-</style>
+</a>
