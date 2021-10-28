138a36a9abdf69fea4bc9fd98c167e037e3b0a5b
Alek <aleksebastian@gmail.com>
2021-10-27 23:35:46 -0500

    Removed unnecessary classes from card and changed flex justify to between

--- src/components/projects/card.svelte
+++ src/components/projects/card.svelte
@@ -5,7 +5,7 @@
 
 <a
 	in:fly={{ y: 200, duration: 1000 }}
-	class="group flex flex-col justify-end w-72 h-72 overflow-hidden relative rounded
+	class="group w-72 h-72 overflow-hidden relative rounded
 		background-white bg-mockup bg-cover bg-no-repeat"
 	style="background-image: url({repo.coverImage});"
 	href={repo.url}
@@ -15,9 +15,9 @@
 >
 	<div />
 	<div
-		class="bg-gradient-to-t from-black w-full h-full xl:top-full group-hover:top-0 transition-top duration-500 ease-in-out absolute flex flex-col justify-end p-4"
+		class="self-end content-end bg-gradient-to-t from-black w-full h-full xl:top-full group-hover:top-0 transition-top duration-500 ease-in-out absolute flex flex-col justify-end p-4"
 	>
-		<h1 class=" text-2xl text-white mb-2">
+		<h1 class="text-2xl text-white mb-2">
 			{repo.name}
 		</h1>
 		<p class=" text-white mb-2">
--- src/components/projects/projects.svelte
+++ src/components/projects/projects.svelte
@@ -7,7 +7,7 @@
 
 <div in:fade class="flex flex-col items-center">
 	<h1 class="text-4xl mb-11 sm:mb-16">Projects</h1>
-	<div class="flex gap-16 justify-center flex-wrap ">
+	<div class="w-11/12 flex justify-between gap-16 flex-wrap">
 		{#each repos as repo}
 			<Card {repo} />
 		{:else}
