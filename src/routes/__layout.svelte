d516672f938f721c6ccca58654f88d5815b2d632
Alek <aleksebastian@gmail.com>
2021-10-28 09:56:50 -0500

    Change main page position on div open to fixed to prevent scrolling except for when page is contact to prevent content from moving off-screen

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,24 @@
 <script>
+	import { onMount } from 'svelte';
+	import { isMobileNavOpen } from '../store.js';
 	import MobileNav from '../components/nav/mobileNav.svelte';
 	import Nav from '../components/nav/nav.svelte';
 	import MediaQuery from '../components/mediaQuery.svelte';
-	import { onMount } from 'svelte';
+
+	import { page } from '$app/stores';
+	$: {
+		current = $page.path;
+	}
+	let current = $page.path;
 
 	let ready = false;
 	onMount(() => (ready = true));
 </script>
 
-<div class="p-8 max-w-6xl mx-auto overflow-x-hidden font-body">
+<div
+	style={$isMobileNavOpen && current !== '/contact' ? 'position:fixed' : 'position:unset'}
+	class="p-8 max-w-6xl mx-auto overflow-x-hidden font-body"
+>
 	{#if ready}
 		<MediaQuery query="(max-width: 480px)" let:matches>
 			{#if matches}
@@ -28,5 +38,9 @@
 
 	:global(*) {
 		-webkit-tap-highlight-color: transparent;
+	}
+
+	.mobileNavOpen {
+		position: fixed;
 	}
 </style>
