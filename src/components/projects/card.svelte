9f0d6abdf3e478c6c15bb78e95d8f903e7ed235b
Alek <aleksebastian@gmail.com>
2021-11-05 22:44:14 -0500

    Refactored card to decrease image brightness instead of a gradient to improve card text legibility

--- src/components/projects/card.svelte
+++ src/components/projects/card.svelte
@@ -13,13 +13,13 @@
 	<img
 		src={repo.coverImage}
 		alt="project mockup thumbnail"
-		class="group w-72 h-72 object-cover object-mockup block"
+		class="group w-72 h-72 object-cover object-mockup block transition duration-500 ease-in-out brightness-50 group-hover:brightness-50 xl:brightness-100"
 		width="300"
 		height="300"
 	/>
 	<div />
 	<div
-		class="self-end content-end bg-gradient-to-t from-black w-full h-full top-0 xl:top-full group-hover:top-0 transition-top duration-500 ease-in-out absolute flex flex-col justify-end p-4"
+		class="self-end content-end w-full h-full top-0 xl:top-full group-hover:top-0 transition-top duration-500 ease-in-out absolute flex flex-col justify-end p-4"
 	>
 		<h1 class="text-2xl text-white mb-2">
 			{repo.name}
