b664821f13822af94aed2d1a5fa419015e2a8930
Alek <aleksebastian@gmail.com>
2021-11-06 19:28:07 -0500

    Installed tailwind plugin for line-clamp in card and added it to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -43,5 +43,6 @@ module.exports = {
 				card: 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px'
 			}
 		}
-	}
+	},
+	plugins: [require('@tailwindcss/line-clamp')]
 };
