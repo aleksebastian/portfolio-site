6f869e3f958121176541dc5230a4879745c05ae3
Alek <aleksebastian@gmail.com>
2021-12-01 23:50:28 -0600

    Fixed button border for light mode

--- src/components/resume/download.svelte
+++ src/components/resume/download.svelte
@@ -7,7 +7,7 @@
 	download="Alek Ortiz resume"
 	rel="noopener"
 	target="_blank"
-	class="flex justify-around items-center w-full bg-gray-800 text-white dark:text-gray-800 dark:bg-white my-8 p-1 border-2 rounded-sm"
+	class="flex justify-around items-center w-full bg-black text-white my-8 p-1 border-2 border-black dark:border-white dark:bg-white dark:text-gray-800 rounded-sm"
 >
 	<div>Download</div>
 	<div class="w-5">
