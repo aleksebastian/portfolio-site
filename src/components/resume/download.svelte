874dc48032f0174af13974408949b4a5ee54a1cf
Alek <aleksebastian@gmail.com>
2021-12-06 18:58:48 -0600

    Resized download button for mobile

--- src/components/resume/download.svelte
+++ src/components/resume/download.svelte
@@ -7,9 +7,9 @@
 	download="Alek Ortiz resume"
 	rel="noopener"
 	target="_blank"
-	class="flex justify-around items-center w-full bg-black text-white my-8 p-1 border-2 border-black dark:border-white dark:bg-white dark:text-gray-800 rounded-sm"
+	class="flex justify-around items-center w-32 md:w-full bg-black text-white my-8 p-1 border-2 border-black dark:border-white dark:bg-white dark:text-gray-800 rounded-sm"
 >
-	<div>Download</div>
+	<p>Download</p>
 	<div class="w-5">
 		<MdFileDownload />
 	</div>
