fb56a08bf0ebdfecda4645ed3225200686fa6615
Alek <aleksebastian@gmail.com>
2022-01-11 08:33:51 -0600

    Added prefers reduced motion accesibility options

--- src/components/projects/projects.svelte
+++ src/components/projects/projects.svelte
@@ -27,3 +27,14 @@
 		</div>
 	{/await}
 </div>
+
+<style>
+	@media (prefers-reduced-motion: reduce) {
+		* {
+			animation-duration: 0.01ms !important;
+			animation-iteration-count: 1 !important;
+			transition-duration: 0.01ms !important;
+			animation-delay: 0.01ms !important;
+		}
+	}
+</style>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -96,3 +96,14 @@
 		</a>
 	</div>
 </div>
+
+<style>
+	@media (prefers-reduced-motion: reduce) {
+		* {
+			animation-duration: 0.01ms !important;
+			animation-iteration-count: 1 !important;
+			transition-duration: 0.01ms !important;
+			animation-delay: 0.01ms !important;
+		}
+	}
+</style>
