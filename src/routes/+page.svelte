396eef43d02f82d754e3b5c449f23500f0936aa4
Alek Ortiz <aleksebastian@gmail.com>
2022-08-18 13:31:03 -0500

    Migration fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
 	import Footer from '../components/projects/footer.svelte';
 	import Projects from '../components/projects/projects.svelte';
-
 	export let data;
+	const { portfolioRepos } = data;
 </script>
 
 <svelte:head>
 	<title>Alek Ortiz | Portfolio</title>
 </svelte:head>
 
-<Projects {data} />
+<Projects {portfolioRepos} />
 <Footer />
