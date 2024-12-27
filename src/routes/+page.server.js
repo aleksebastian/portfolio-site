5f099aa180b1e680bb87967265d8ab1a1599b54f
Alek Ortiz <aleksebastian@gmail.com>
2024-12-27 11:49:35 -0600

    Use authenticated GH API

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,28 @@
-import { error } from '@sveltejs/kit';
+import { GITHUB_TOKEN } from '$env/static/private';
 
-/** @type {import('@sveltejs/kit').PageServerLoad} */
+async function fetchRepositories(username) {
+	const url = `https://api.github.com/users/${username}/repos`;
+	const res = await fetch(url, {
+		headers: {
+			Authorization: `token ${GITHUB_TOKEN}`,
+			'X-GitHub-Api-Version': '2022-11-28'
+		}
+	});
+
+	if (!res.ok) {
+		throw new Error(JSON.stringify(res));
+	}
+
+	const data = await res.json();
+	return data;
+}
+
 export async function load({ setHeaders }) {
 	const username = 'aleksebastian';
-	const url = `https://api.github.com/users/${username}/repos`;
-	const res = await fetch(url);
-	if (res.ok) {
-		const data = await res.json();
-		const portfolioRepos = data
+
+	try {
+		const repos = await fetchRepositories(username);
+		const portfolioRepos = repos
 			.filter((repo) => repo.topics.includes('portfolio-project'))
 			.map((repo) => ({
 				username: username,
@@ -20,23 +35,25 @@ export async function load({ setHeaders }) {
 			}));
 
 		setHeaders({
-			'Cache-Control': `max-age=0, s-max-age=${300}` // 5 minutes
+			'cache-control': 'public, max-age=1800' // 30 minutes
 		});
 
 		return { portfolioRepos };
-	} else {
+	} catch (err) {
+		const resError = JSON.parse(err.message);
+
 		const errorCard = [
 			{
 				username: username,
-				name: `Bummer! Got a ${res.status} while fetching my projects from github`,
-				url: 'https://github.com/aleksebastian',
+				name: `Bummer! Got a ${resError.status} while fetching my projects from github`,
 				homepage: '',
+				url: 'https://github.com/aleksebastian',
 				description:
 					'While things come back online, feel free to click on the link below to go see my projects Github profile',
 				coverImage: `https://res.cloudinary.com/blitva/image/upload/v1635379036/Project%20screenshots/error_mdjl8k.webp`
 			}
 		];
 
-		throw error(500, errorCard);
+		return { errorCard };
 	}
 }
