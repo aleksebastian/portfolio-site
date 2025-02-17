480a9390dc794461648e0b7af8c86db22a4bdd8b
Alek Ortiz <aleksebastiano@gmail.com>
2025-02-17 15:09:57 -0600

    Check if icons exist to prevent displaying empty link elements

--- src/routes/components/Projects/Card.svelte
+++ src/routes/components/Projects/Card.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Icon from '@iconify/svelte';
+	import Icon, { iconExists } from '@iconify/svelte';
 	let { repo } = $props();
 </script>
 
@@ -14,18 +14,19 @@
 		width="300"
 		height="300"
 	/>
-	<div></div>
 	<div
 		class="self-end content-end w-full h-full top-0 xl:top-[85%] xl:group-hover:-translate-y-[85%] transition-tranform duration-500 ease-in-out absolute flex flex-col justify-between motion-reduce:transition-none motion-reduce:transform-none will-change-transform bg-[rgba(0,0,0,.45)]"
 	>
 		<div class="transition duration-[250ms] ease-in-out h-12 flex items-center px-4 gap-4">
-			{#if repo.topics.length}
-				{#each repo.topics as topic}
-					<a title={topic} href={`https://www.google.com/search?q=${topic}`} target="_blank">
-						<Icon icon={`simple-icons:${topic}`} height="27.5" color="white" />
+			{#each repo.topics as topic}
+				{@const iconString = `simple-icons:${topic}`}
+				{#if iconExists(iconString)}
+					{@const googleSearchUrl = `https://www.google.com/search?q=${topic}`}
+					<a title={topic} href={googleSearchUrl} target="_blank">
+						<Icon icon={iconString} height="27.5" color="white" />
 					</a>
-				{/each}
-			{/if}
+				{/if}
+			{/each}
 		</div>
 		<div class="p-4">
 			<h1 class="text-2xl text-white mb-1">
@@ -34,7 +35,7 @@
 			<p class=" text-white mb-2 line-clamp-5">
 				{repo.description}
 			</p>
-			<!-- <p class="text-sm text-gray-400 -mb-2">Click to go to Github repo</p> -->
+			<!-- <p class="text-sm text-gray-400 -mb-2">Click to go to Github repo</p> -->p
 			<div class="flex gap-4 py-1">
 				{#if repo.homepage}
 					<a
